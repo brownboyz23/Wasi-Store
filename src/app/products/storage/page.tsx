5b868a2314d3f42d084bae091857eecec9d00aaf
wasikali <wasikali443@gmail.com>
2026-07-21 21:24:04 +0500

    csseee19

--- src/app/products/storage/page.tsx
+++ src/app/products/storage/page.tsx
@@ -76,7 +76,7 @@ const Storage = () => {
       <div className='flex  justify-center items-center mt-25 '>
         <div className='py-0  border-2 '>
           {products && ((
-            <div className=' w-full min-h-screen mb-15 CardXrp '>
+            <div className=' w-full min-h-screen mb-5 CardXrp '>
               {
                 filterd.map((items, index) => (
                   <Card key={index} className='w-[200px] min-h-[320px] py-0  border white shadow-lg CardResp' onClick={() => setHoveredProducts(items)}>
